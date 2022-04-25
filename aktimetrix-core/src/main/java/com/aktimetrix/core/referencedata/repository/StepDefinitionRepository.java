21cd42965422f80a78f07d1bd981b8b04b7d8365
arun406 <k.arun406@gmail.com>
2022-04-25 16:21:37 +0400

    initial commit

--- aktimetrix-core/src/main/java/com/aktimetrix/core/referencedata/repository/StepDefinitionRepository.java
+++ aktimetrix-core/src/main/java/com/aktimetrix/core/referencedata/repository/StepDefinitionRepository.java
@@ -3,10 +3,12 @@ package com.aktimetrix.core.referencedata.repository;
 import com.aktimetrix.core.referencedata.model.StepDefinition;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  *
  */
+@RepositoryRestResource(itemResourceRel = "step-definition", collectionResourceRel = "step-definitions", path = "step-definitions")
 public interface StepDefinitionRepository extends MongoRepository<StepDefinition, String> {
 
     /**
